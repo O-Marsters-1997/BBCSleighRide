f464b0817a0b2252889545437e13fc806bc51365
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-04-13 19:17:01 +0100

    added deployed heroku link to readme

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 BBC Sleigh Ride is a prototype of a Christmas-themed educational app for primary-aged children; fictitiously commissioned by the BBC. It offers interactive learning and testing of Christmas-related facts from around the world, as well as some festive entertainment features.
 
 A demo is available here: https://youtu.be/im-5jjxNOzk
+Alternatively a fully deployed version of the frontend application can be found at the following link if you want to have a go with some of the features.
+https://musing-panini-a7dc6f.netlify.app/
 
 ![BBCSleighRide](screenshots/BBCSleighRide_homepage.png)
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-require('dotenv').config
+require("dotenv").config;
 
 app.use(express.json());
 const MongoClient = require("mongodb").MongoClient;
-const createRouter = require('./helpers/create_router.js');
-
+const createRouter = require("./helpers/create_router.js");
 
 MongoClient.connect(
   "mongodb+srv://OMarsters-1997:Themarster97@cluster0.xmyq2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -21,15 +20,13 @@ MongoClient.connect(
     const jokesRouter = createRouter(jokesCollection);
     const quizCollection = db.collection("quiz");
     const quizRouter = createRouter(quizCollection);
-    app.use("/api/countries", countriesRouter);
-    app.use("/api/jokes", jokesRouter);
-    app.use("/api/quiz", quizRouter);
+    app.use("/countries", countriesRouter);
+    app.use("/jokes", jokesRouter);
+    app.use("/quiz", quizRouter);
   })
 
   .catch(console.err);
 
-
 app.listen(process.env.PORT || 5000, function () {
-    console.log(`Listening on port ${ this.address().port }`);
-  });
-
+  console.log(`Listening on port ${this.address().port}`);
+});
