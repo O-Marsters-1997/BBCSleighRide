b550b843142844cbd9e7bb0eaeb421a05b2137b1
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-10-22 14:19:25 +0100

    finished eslint config for test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("All basic react tests", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test("renders learn react link", () => {
+    const linkElement = screen.getByText("hello world");
+    expect(linkElement).toBeInTheDocument();
+  });
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 
-const items = new Array();
-const greeting = "name";
-
-const App = () => {
-  return <div>hello world</div>;
-};
+const App = () => <div>hello world</div>;
 
 export default App;
