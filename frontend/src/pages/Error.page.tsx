4c13e8d64358f3cb43d3af02e332955bc5f21aa8
O-Marsters-1997 <olly.marsters@yahoo.co.uk>
2022-11-18 13:33:54 +0000

    error page done

--- frontend/src/pages/Error.page.tsx
+++ frontend/src/pages/Error.page.tsx
@@ -3,20 +3,48 @@ import styled from "styled-components";
 import View from "../components/View";
 import Image from "../components/Image";
 import { RowContainer, CentralColumnContainer } from "../components/Lib";
+import { useViewport } from "../hooks/useViewport";
 import error_title from "../assets/images/page_naughty_list.svg";
 import error_image from "../assets/images/404_page.svg";
 
 const StyledView = styled(View)``;
 
-const Error = () => (
-  <StyledView>
-    <RowContainer component="section">
+const Error = () => {
+  const viewport = useViewport();
+  return (
+    <StyledView>
       <CentralColumnContainer>
-        <Image src={error_title} alt="404" />
-        <Image src={error_image} alt="404" />
+        <RowContainer component="section">
+          <View
+            style={
+              viewport("medium")
+                ? { padding: "8em 0 6em 0" }
+                : { padding: "6em 0 2em 0" }
+            }
+          >
+            <Image
+              src={error_title}
+              alt="404"
+              width={
+                viewport("smallMedium") ? "clamp(550px, 55vw, 1100px)" : "350px"
+              }
+              height="fit-content"
+            />
+          </View>
+        </RowContainer>
+        <RowContainer>
+          <Image
+            src={error_image}
+            alt="404"
+            width={
+              viewport("smallMedium") ? "clamp(450px, 45vw, 950px)" : "350px"
+            }
+            height="fit-content"
+          />
+        </RowContainer>
       </CentralColumnContainer>
-    </RowContainer>
-  </StyledView>
-);
+    </StyledView>
+  );
+};
 
 export default Error;
