719022ea9b25b2bcde9fc411c62a7c6c47fdbce8
Olly Marsters <ollymarsters@yahoo.co.uk>
2022-11-27 13:09:47 +0000

    testing lint staged on server

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || "";
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || "";
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD ?? "";
 
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.xmyq2.mongodb.net/sleigh_ride`;
 
